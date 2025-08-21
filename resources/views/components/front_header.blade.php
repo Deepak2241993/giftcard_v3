f2a2dfe2faf168b503e244b1098dcc8715ffce8e
Deepak Prasad <deepakprasad224@gmail.com>
2025-08-21 13:33:29 +0530

    update

--- resources/views/components/front_header.blade.php
+++ resources/views/components/front_header.blade.php
@@ -145,7 +145,7 @@
         <nav class="header-nav">
             <a href="https://forevermedspanj.com/" class="nav-link">Website</a>
             <a href="{{ url('/') }}" class="nav-link">Giftcards</a>
-            <a href="{{ route('services') }}" class="nav-link">Services</a>
+            {{-- <a href="{{ route('services') }}" class="nav-link">Services</a> --}}
             @if (Session::get('patient_details'))
                 <a class="nav-link"
                     href="{{ route('patient-dashboard') }}">{{ Auth::guard('patient')->user()->fname }}</a>
